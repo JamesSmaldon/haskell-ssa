b02ae2054ef89c96b1d765b0ae9501b0d6d5517c
James Smaldon <James.Smaldon@gmail.com>
2013-07-12 00:13:49 +0100

    Added some basic charting

--- SSA.hs
+++ SSA.hs
@@ -1,18 +1,24 @@
 import qualified Data.Map as M
+import qualified Data.Set as S
 import Data.List
 import Control.Monad
 import Control.Monad.Random
 import Debug.Trace
 import Control.Applicative
 import Data.Maybe
+import Data.Colour.Names
+import Data.Colour
+import Data.Accessor
+import Graphics.Rendering.Chart
+import Graphics.Rendering.Chart.Gtk
 
 -- make things a bit more readable
 type Name = String
 type Count = Int
-type Rate = Float
-type Propensity = Float
-type PropensitySum = Float
-type Time = Float
+type Rate = Double
+type Propensity = Double
+type PropensitySum = Double
+type Time = Double
 type Input = Chemical
 type Output = Chemical
 
@@ -42,7 +48,7 @@ selectReaction rps target = liftM fst . find gtTarget . scanl1 sumTuple $ rps
                                 where sumTuple (a,b) (c,d) = (c, d+b)
                                       gtTarget (_, x) = x > target
 
-calcTimeInc :: PropensitySum -> Float -> Time
+calcTimeInc :: PropensitySum -> Double -> Time
 calcTimeInc propSum rnum = -log rnum / propSum
 
 mapadj f = M.adjust f
@@ -55,12 +61,12 @@ react (System m) (Assoc r i o) = System $ mapadj (succ.succ) o (mapadj (pred.pre
 react (System m) (Disassoc r i o) = react (System m) (Assoc r o i)
 
 
-nextReaction' :: [Reaction] -> Float -> Float -> System -> Maybe (Reaction, Time)
+nextReaction' :: [Reaction] -> Double -> Double -> System -> Maybe (Reaction, Time)
 nextReaction' rs rnum1 rnum2 s = pure (,) <*> selectReaction ps (rnum1 * propSum) <*> pure (calcTimeInc propSum rnum2)
                                     where ps = calcPropensities s rs
                                           propSum = sum (map snd ps)
 
-doNextReaction :: (System, Time) -> [Reaction] -> Time -> Float -> Float -> Maybe (System, Time)
+doNextReaction :: (System, Time) -> [Reaction] -> Time -> Double -> Double -> Maybe (System, Time)
 doNextReaction st@(s, now) rs end rnum1 rnum2 = nextReaction' rs rnum1 rnum2 s >>= doReaction
                                                 where doReaction (r, deltat) = if (now + deltat) <= end 
                                                                                     then Just (react s r, now + deltat) 
@@ -71,19 +77,39 @@ run st rs stopTime = liftM2 doNextReaction' getRandom getRandom >>= stopIfNoReac
                         where stopIfNoReaction = maybe (return []) (\x -> liftM2 (:) (return x) (run x rs stopTime))
                               doNextReaction' = doNextReaction st rs stopTime
 
-tabulateOutput :: [(System, Time)] -> String
-tabulateOutput = unlines . (map $ show . M.toList . getCounts . fst)
-                    where getCounts (System m) = m
+namedTimeSeries :: [(System, Time)] -> [(Chemical, [(Time, Double)])]
+namedTimeSeries xs = M.toList $ foldr addTimes M.empty xs
+                       where addTimes (System m, time) m2 = foldr (insertDataPt time) m2 (M.toList m)   
+                             insertDataPt time (chem, cnt)  = M.insertWith (++) chem [(time, fromIntegral cnt)] 
+
+
+
+createPlotLines :: [(Chemical, [(Time, Double)])] -> [AlphaColour Double] -> [Either (Plot Time Double) (Plot Time Double)]
+createPlotLines xs cols = map createPlotLine $ zip xs cols
+                where createPlotLine ((Chemical n, ts),col) = Left $ toPlot $ plot_lines_style .> line_color ^= col
+                                                           $ plot_lines_values ^= [ts]
+                                                           $ plot_lines_title ^= n 
+                                                           $ defaultPlotLines
 
 c1 = Chemical "A"
 c2 = Chemical "B"
+c3 = Chemical "C"
 r1 = FstOrd 0.5 c1 c2
 r2 = FstOrd 0.1 c2 c1
-rs = [r1, r2]
+r3 = SndOrd 0.2 c1 c2 c3
+r4 = FstOrd 0.1 c3 c1
+rs = [r1, r2, r3, r4]
 
-s = System (M.fromList [(c1, 100), (c2, 300)])
+s = System (M.fromList [(c1, 300), (c2, 0), (c3, 0)])
+
+chart nts = layout 
+  where
+    layout = layout1_title ^="SSA"
+           $ layout1_plots ^= createPlotLines nts [opaque blue, opaque green, opaque red]
+           $ layout1_grid_last ^= False
+           $ defaultLayout1
 
 main :: IO ()
 main = do 
-        let output = tabulateOutput $ evalRand (run (s, 0.0) rs 100.0) (mkStdGen 1)
-        putStr output
+        let output = namedTimeSeries $ evalRand (run (s, 0.0) rs 100.0) (mkStdGen 1)
+        renderableToWindow (toRenderable $ chart output) 640 480
